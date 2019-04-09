76a5a8fe3a4239f3509f6ae5728557a051c01b29
Shanmugapriya03 <priya97a@gmail.com>
2019-04-10 00:48:22 +0530

    added logic for the component to consume the service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { GameEngineService } from './game-engine.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Trivia';
+  objectKeys = Object.keys;
+  constructor(private service : GameEngineService){}
+
+  quizSet = [];
+
+  ngOnInit(){
+    this.service.getQuestions()
+        .subscribe(
+          response => {
+            this.quizSet = JSON.parse(JSON.stringify(response['results']));
+            console.log(this.quizSet);
+          }
+        );
+  }
 }
